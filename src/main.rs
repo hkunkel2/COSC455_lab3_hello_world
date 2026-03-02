f5d610c71f2e0768423c8af00fa72795dde5cb8c
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 16:31:01 -0500

    multi conditional

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,23 @@ fn main() {
     // chapter4_challenge::main();
 
     let x = 3;
+    let y = 5;
 
-    if x + 1 != 3 {
-        println!("x + 1 is NOT 3");
+    if x > y {
+        println!("x is greater than y")
+    } else {
+        if x == y {
+            println!("x is equal to y")
+        } else {
+            println!("x is less than to y")
+        }
+    }
+
+    if x > y {
+        println!("x is greater than y")
+    } else if x == y {
+            println!("x is equal to y")
+    } else {
+            println!("x is less than to y")
     }
 }
