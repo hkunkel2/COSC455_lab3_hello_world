b1945ceed991b5f92aad450df1a910066860ad81
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 16:27:39 -0500

    if statement

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,11 @@ mod chapter4_challenge;
 
 fn main() {
     // chapter2_challenge::main();
-    chapter4_challenge::main();
+    // chapter4_challenge::main();
+
+    let x = 3;
+
+    if x + 1 != 3 {
+        println!("x + 1 is NOT 3");
+    }
 }
