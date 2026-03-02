63d7770e7a988a42be4a8af825914c513772212e
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 11:29:08 -0500

    integer type are interesting

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ mod task5;
 
 fn main() {
     // this is my hello world program comment
-    let mut x = 10;
-    println!("x is {}", x);
-    x = 20;
+    let mut x: u8 = 255; // must be 0-255
     println!("x is {}", x);
     // task5::main();
 }
