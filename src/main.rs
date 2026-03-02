4c3716dcab37a4ca0da8394abe0bf5b0373de7f2
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 16:13:23 -0500

    expression and statements

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,12 @@ mod chatper2_challenge;
 
 fn main() {
     // chatper2_challenge::main();
-    say_hello();
-    say_a_number(23);
+    let result = square(13);
+    println!("result is {:?}",result)
 
-    let x = 1;
-    let y = 2;
-
-    say_a_sum(x, y);
-    say_a_number(x as i32);
 }
 
-fn say_hello() {
-    println!("Hello!")
-}
-
-fn say_a_number(number: i32) {
-    println!("number is {}", number);
-}
-
-fn say_a_sum(num1: u8, num2: u8) {
-    println!("sum is {}", num1 + num2);
+fn square(x: i32) -> (i32, i32) {
+    println!("sqauring {}", x);
+    (x, x * x)
 }
