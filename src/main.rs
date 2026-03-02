a0a375e4e13fd81b30dd7adbff720ba8166ba346
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 11:40:58 -0500

    floating point fun

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod task5;
 
 fn main() {
     // this is my hello world program comment
-    let mut x: u8 = 255; // must be 0-255
+    let mut x: f32 = 10.2346544924355423; // must be 0-255
     println!("x is {}", x);
-    // task5::main();
 }
