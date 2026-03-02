6814d8146869ddfe9672a0b5d4d7ec1688e4cad0
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 15:49:25 -0500

    arrays

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 mod chatper2_challenge;
-mod task5;
 
 fn main() {
-    chatper2_challenge::main();
+    // chatper2_challenge::main();
+    let mut letters = ['a', 'b', 'c'];
+    letters[0] = 'x';
+    let first_letter = letters[0];
+    println!("first_letter is {}", first_letter);
+
+    let numbers: [i32; 5];
+    numbers = [0; 5];
+    let index = numbers.len();
+    println!("last number is {}", numbers[index - 1]);
 }
