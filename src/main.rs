da85a2a0a4565acfb46fffa155b51ceefd1ce2d4
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 15:53:00 -0500

    multi dem arrays

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,10 @@ mod chatper2_challenge;
 
 fn main() {
     // chatper2_challenge::main();
-    let mut letters = ['a', 'b', 'c'];
-    letters[0] = 'x';
-    let first_letter = letters[0];
-    println!("first_letter is {}", first_letter);
+    let parking_lot =   [[1, 2, 3],
+                                        [4, 5, 6]];
+    let number = parking_lot[1][2];
+    println!("number is {}", number);
 
-    let numbers: [i32; 5];
-    numbers = [0; 5];
-    let index = numbers.len();
-    println!("last number is {}", numbers[index - 1]);
+    let garage: [[[0; 100]; 20]; 5];
 }
