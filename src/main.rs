93c18b28cbf38b79729ee7b0baa3401bb8c786ee
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 10:58:12 -0500

    Multiline comments are more fun

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,25 @@ mod task5;
 
 fn main() {
     // this is my hello world program comment
+    /* 
+    M
+    U
+    L
+    T
+    I
+    L
+    I
+    N
+    E
+    
+    C
+    O
+    M
+    M
+    E
+    N
+    T
+     */
     println!("Hello, world!");
     // comment out task five since we dont need to run it anymore 
     // task5::main();
