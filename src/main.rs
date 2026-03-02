55b777305906c5654472c36223829541c2eaf0cd
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 11:23:24 -0500

    good time declaring vars

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,9 @@ mod task5;
 
 fn main() {
     // this is my hello world program comment
-    /* 
-    M
-    U
-    L
-    T
-    I
-    L
-    I
-    N
-    E
-    
-    C
-    O
-    M
-    M
-    E
-    N
-    T
-     */
-    println!("Hello, world!");
-    // comment out task five since we dont need to run it anymore 
+    let mut x = 10;
+    println!("x is {}", x);
+    x = 20;
+    println!("x is {}", x);
     // task5::main();
 }
