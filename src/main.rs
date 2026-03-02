a0e1da1839cff2d01b3577465ce89634bcc3bbe4
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 17:12:33 -0500

    nested loops

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,14 @@ fn main() {
     // chapter2_challenge::main();
     // chapter4_challenge::main();
 
-    let message = ['h','e','l','l','o'];
-
-    for (index, &item) in message.iter().enumerate() {
-        println!("item {} is {}", index, item);
-        if item == 'e' {
-            break;
+    let mut matrix = [[1,2,3],
+                                    [4,5,6],
+                                    [7,8,9]];
+    for row in matrix.iter_mut() {
+        for num in row.iter_mut() {
+            *num += 10;
+            print!("{}\t", num);
         }
-    }
-
-    for number in 0..5 {
-        println!("number is {}", number)
+        println!();
     }
 }
