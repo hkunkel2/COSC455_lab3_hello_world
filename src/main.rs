9909e09b639cd6e5b9c943c1df78b62f0ca09c6c
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 15:18:34 -0500

    bitwise operations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
-    // this is my hello world program comment
-    let a = 10;
-    let b = 3.0;
-    let c = a as f64 / b;
-    println!("c is {0:08.3}\na is {1}\nonce again, c is {0}", c, a);
+    let value = 0b1111_0101u8;
+    println!("value is {}", value);
+    println!("value is {:08b}", value);
+    println!("not value is {:08b}", !value);
+    println!("value but reset bit in position 2 is {:08b}", value & 0b1111_1011);
+    println!("value if bit 6 is {}", value & 0b0100_0000);
+    println!("set position 4 to 1 is {:08b}", value | 0b0000_1000);
+    println!("set position 4 to 1 is {:08b}", value ^ 0b0000_1000);
+    println!("set position 4 to 1 is {:08b}", value << 2);
+    println!("set position 4 to 1 is {:08b}", value >> 3);
 }
