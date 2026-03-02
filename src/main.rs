fb496e88ff9ae9a2ab3680f7284e6d02f30c7bfe
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 16:20:16 -0500

    chapter 4 challenge

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-mod chatper2_challenge;
+mod chapter2_challenge;
+mod chapter4_challenge;
 
 fn main() {
-    // chatper2_challenge::main();
-    let result = square(13);
-    println!("result is {:?}",result)
-
-}
-
-fn square(x: i32) -> (i32, i32) {
-    println!("sqauring {}", x);
-    (x, x * x)
+    // chapter2_challenge::main();
+    chapter4_challenge::main();
 }
