e7d5a21ecf67d165b9bea00356325daff2e00fc0
hkunkel2 <harrisonjameskunkel@gmail.com>
2026-03-02 17:28:11 -0500

    chapter 2, 4 and 5 challenge

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod chapter4_challenge;
 mod chapter5_challenge;
 
 fn main() {
-    // chapter2_challenge::main();
-    // chapter4_challenge::main();
+    chapter2_challenge::main();
+    chapter4_challenge::main();
     chapter5_challenge::main();
 }
